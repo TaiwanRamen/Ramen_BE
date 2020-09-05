c2c8a08e255fe4c9bbf23e29ec534fc662ce4d40
JulianLin549 <sres3416@gmail.com>
2020-09-05 23:45:03 +0800

    update mongo address

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 //DB Config
 //process.env.DATABASE_ATLAS = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PWD + '@cluster0.qzive.mongodb.net/<dbname>?retryWrites=true&w=majority'
-//process.env.DATABASE = 'mongodb://localhost:27017/acc_activate'
+//process.env.DATABASE_LOCAL = 'mongodb://localhost:27017/ramen_group'
 
 
-mongoose.connect(process.env.DATABASE, {
+mongoose.connect(process.env.DATABASE_ATLAS, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
