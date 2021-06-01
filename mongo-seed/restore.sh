1714ee1a7ad83e48cb4dcdcc9863a212d43025a5
JulianLin549 <sres3416@gmail.com>
2021-06-02 02:18:52 +0800

    comment

--- mongo-seed/restore.sh
+++ mongo-seed/restore.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
-mongoimport --host mongodb --db ramen_group --collection comments --type json --file /mongo-seed/dump/initComments.json --jsonArray
-mongoimport --host mongodb --db ramen_group --collection notifications --type json --file /mongo-seed/dump/initNotifications.json --jsonArray
-mongoimport --host mongodb --db ramen_group --collection reviews --type json --file /mongo-seed/dump/initReviews.json --jsonArray
-mongoimport --host mongodb --db ramen_group --collection stores --type json --file /mongo-seed/dump/initStores.json --jsonArray
-mongoimport --host mongodb --db ramen_group --collection users --type json --file /mongo-seed/dump/initUsers.json --jsonArray
+#mongoimport --host="mongo1:27017,mongo2:27017,mongo3:27017" --db ramen_group --collection comments --type json --file /mongo-seed/dump/initComments.json --jsonArray
+#mongoimport --host="mongo1:27017,mongo2:27017,mongo3:27017" --db ramen_group --collection notifications --type json --file /mongo-seed/dump/initNotifications.json --jsonArray
+#mongoimport --host="mongo1:27017,mongo2:27017,mongo3:27017" --db ramen_group --collection reviews --type json --file /mongo-seed/dump/initReviews.json --jsonArray
+#mongoimport --host="mongo1:27017,mongo2:27017,mongo3:27017" --db ramen_group --collection stores --type json --file /mongo-seed/dump/initStores.json --jsonArray
+#mongoimport --host="mongo1:27017,mongo2:27017,mongo3:27017" --db ramen_group --collection users --type json --file /mongo-seed/dump/initUsers.json --jsonArray
+
+
+
+_wait "mongo2"
+_wait "mongo3"
+echo "Starting to import data..."
+mongoimport --host="mongo1:27017,mongo2:27017,mongo3:27017" --db ramen_group --collection users --type json --file /mongo-seed/dump/initUsers.json --jsonArray
+echo "Done."
