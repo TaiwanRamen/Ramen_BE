acc516b152cee9570fb4ff435cd06bef14d86bb3
JulianLin549 <sres3416@gmail.com>
2021-06-12 20:52:09 +0800

    and update findOneAndUpdate in comments and management to ensure atomic

--- ramen-application/src/api/v1/management.js
+++ ramen-application/src/api/v1/management.js
@@ -16,27 +16,21 @@ router.post('/registerStoreOwner', middleware.jwtAuth, middleware.isAdmin, dataV
             session.startTransaction();
 
             let storeId = req.body.storeId;
-            let storeOwnerId = req.body.storeOwnerId;
+            let userId = req.body.storeOwnerId;
 
-            const storeRelation = await StoreRelation.findOne({'storeId': storeId}).session(session);
-            let user = await User.findById(req.body.storeOwnerId).session(session);
+            const user = await User.findOneAndUpdate(
+                {'_id': userId},
+                {$addToSet: {hasStore: new mongoose.Types.ObjectId(storeId)}},
+                {session: session}
+            );
+
+            const storeRelation = await StoreRelation.findOneAndUpdate(
+                {'storeId': storeId},
+                {$addToSet: {owners: new mongoose.Types.ObjectId(userId)}},
+                {session: session}
+            );
 
             if (!storeRelation || !user) throw new Error("找不到使用者或是店家")
-
-            //add to set
-            // db.inventory.update(
-            //     { _id: 1 },
-            //     { $addToSet: { tags: "accessories" } }
-            // )
-            if (!storeRelation.owners.includes(storeOwnerId) && !user.hasStore.includes(storeId)) {
-                storeRelation.owners.push(storeOwnerId);
-                user.hasStore.push(storeId);
-            } else {
-                throw new Error("使用者已是店家管理員");
-            }
-
-            await storeRelation.save({session: session});
-            await user.save({session: session});
 
             await session.commitTransaction()
             session.endSession()
@@ -58,25 +52,26 @@ router.delete('/removeStoreOwner', middleware.jwtAuth, middleware.isAdmin, dataV
             session.startTransaction();
 
             const storeId = req.body.storeId;
-            const storeOwnerId = req.body.storeOwnerId;
+            const userId = req.body.storeOwnerId;
 
-            const storeRelation = await StoreRelation.findOne({'storeId': storeId}).session(session);
-            let user = await User.findById(req.body.storeOwnerId).session(session);
+            const storeRelation = await StoreRelation.findOneAndUpdate(
+                {'storeId': storeId},
+                {$pull: {owners: new mongoose.Types.ObjectId(userId)}},
+                {multi: false, session: session}
+            );
+
+            let user = await User.findOneAndUpdate(
+                {'_id': userId},
+                {$pull: {hasStore: new mongoose.Types.ObjectId(storeId)}},
+                {multi: false, session: session}
+            );
 
             if (!storeRelation || !user) throw new Error("找不到使用者或是店家")
 
-            let userHasStoreIndex = user.hasStore.indexOf(storeId);
-            let storeOwnerIndex = storeRelation.owners.indexOf(storeOwnerId);
 
-            if (userHasStoreIndex > -1 && storeOwnerIndex > -1) {
-                user.hasStore.splice(userHasStoreIndex, 1);
-                storeRelation.owners.splice(storeOwnerIndex, 1);
-                await user.save({session: session});
-                await storeRelation.save({session: session});
-            }
             await session.commitTransaction()
             session.endSession()
-            log.info("removed owner {} from store {}", storeOwnerId, storeId)
+            log.info(`removed owner ${userId} from store ${storeId}`)
             return response.success(res);
 
         } catch (error) {
